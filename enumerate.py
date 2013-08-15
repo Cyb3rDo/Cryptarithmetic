7585418670b7608d1d7ba216a4939056d7f17a09
Sukhvinder Singh <karramsos@gmail.com>
2013-08-15 20:25:28 +0200

    First commit

---
+++ enumerate.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# enumerate(sequence)
+
+for (i, d) in enumerate("ONE"):
+	print(i,d)
+	print '{}*{}'.format(10**i, d)
+	print ""
---
+++ lambda_quiz.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+d = lambda p: p * 2 
+t = lambda p: p * 3 
+x = 2 
+x = d(x)
+x = t(x) 
+x = d(x)
+print x
+
